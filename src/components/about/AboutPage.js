0d27ade77bb59c980de61245e619ed8b9f66652b
Roger King <rogr.king@gmail.com>
2017-04-30 09:53:44 -0400

    initial commit - start

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1> About </h1>
+        <p> This Application uses React, Redux, React Router and other stuff.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,58 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from './../../actions/courseActions';
+
+class CoursesPage extends React.Component{
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+      course: { title: null }
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+  onClickSave(){
+    this.props.createCourse(this.state.course);
+  }
+
+  courseRow(course, index){
+    return <div key={index}>{course.title}</div>;
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2> Add Courses</h2>
+        <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
+        <input type="submit"  onClick={this.onClickSave} />
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state, ownProps){
+  return{
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
